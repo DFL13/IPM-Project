e980f673299ad46a2221132933da74d76c639cbd
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-03-31 16:22:19 +0100

    Added post's info

--- prot1/data.js
+++ prot1/data.js
@@ -8,14 +8,50 @@ var posts = [{
 			}, 
 			{
 				person: "Tony Stark",
-				place: "Paris, France",
+				place: "Kraków, Poland",
 				image: "img/posts/jacek-dylag-579738-unsplash.jpg",
 				upvotes: 105,
 				date: "3 MAR 2019",
 				comments: [{person: "Jon Snow", message: "Welcome to the night's watch my brother. Winter is coming."}]
+			},
+			{
+				person: "Peter Parker",
+				place: "Moscow, Russia",
+				image: "img/posts/nikolay-vorobyev-470715-unsplash.jpg",
+				upvotes: 26,
+				date: "01 MAR 2018",
+				comments: [{person: "Peter Parker", message: "Well, wobble my webs and call me shaky!"}]
+			},
+			{
+				person: "Johnny Depp",
+				place: "Agueda, Portugal",
+				image: "img/posts/ricardo-resende-803794-unsplash.jpg",
+				upvotes: 206,
+				date: "02 SEP 2018",
+				comments: [{person: "Johnny Depp", message: "Not all treasure is silver and gold, mate."}]
+			},
+			{
+				person: "Ryan Reynolds",
+				place: "Santorini, Greece",
+				image: "img/posts/margaret-barley-42-unsplash.jpg",
+				upvotes: 596,
+				date: "09 JUL 2013",
+				comments: [{person: "Ryan Reynolds", message: "I can say Chimichanga in seven languages."}]
+			},
+			{
+				person: "Harry Potter",
+				place: "Majorca, Spain",
+				image: "img/posts/benjamin-voros-151769-unsplash.jpg",
+				upvotes: 596,
+				date: "19 OCT 2016",
+				comments: [{person: "Harry Potter", message: "I solemnly swear i am up to no good."}]
 			}];
 
 var people = {
 				"Jon Snow": {pic: "img/posts/david-marcu-4654-unsplash.jpg", quote: "", posts: []},
-				"Tony Stark": {pic: "img/posts/david-marcu-4654-unsplash.jpg", quote: "", posts: []}
+				"Tony Stark": {pic: "img/posts/david-marcu-4654-unsplash.jpg", quote: "", posts: []},
+				"Peter Parker": {pic: "img/posts/david-marcu-4654-unsplash.jpg", quote: "", posts: []},
+				"Johnny Depp": {pic: "img/posts/david-marcu-4654-unsplash.jpg", quote: "", posts: []},
+				"Ryan Reynolds": {pic: "img/posts/david-marcu-4654-unsplash.jpg", quote: "", posts: []},
+				"Harry Potter": {pic: "img/posts/david-marcu-4654-unsplash.jpg", quote: "", posts: []}
 			};
